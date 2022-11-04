785f5298e5d347326b0b93cc2e9efae700332f42
backinfile <1757518497@qq.com>
2022-11-04 18:31:38 +0800

    补充function

---
+++ src/main/java/com/backinfile/support/func/CommonFunction.java
@@ -0,0 +1,38 @@
+package com.backinfile.support.func;
+
+import com.backinfile.support.SysException;
+
+public class CommonFunction {
+    private final int argNum;
+    private final Object func;
+
+    public CommonFunction(int argNum, Object func) {
+        this.argNum = argNum;
+        this.func = func;
+    }
+
+    @SuppressWarnings("all")
+    public Object invoke(Object... args) {
+        switch (argNum) {
+            case 0:
+                return ((Function0) func).invoke();
+            case 1:
+                return ((Function1) func).invoke(args[0]);
+            case 2:
+                return ((Function2) func).invoke(args[0], args[1]);
+            case 3:
+                return ((Function3) func).invoke(args[0], args[1], args[2]);
+            case 4:
+                return ((Function4) func).invoke(args[0], args[1], args[2], args[3]);
+            case 5:
+                return ((Function5) func).invoke(args[0], args[1], args[2], args[3], args[4]);
+            case 6:
+                return ((Function6) func).invoke(args[0], args[1], args[2], args[3], args[4], args[5]);
+            case 7:
+                return ((Function7) func).invoke(args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
+            case 8:
+                return ((Function8) func).invoke(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]);
+        }
+        throw new SysException("argNum not march");
+    }
+}
---
+++ src/main/java/com/backinfile/support/func/Function3.java
@@ -0,0 +1,6 @@
+package com.backinfile.support.func;
+
+@FunctionalInterface
+public interface Function3<T1, T2, T3, R> {
+    R invoke(T1 t1, T2 t2, T3 t3);
+}
